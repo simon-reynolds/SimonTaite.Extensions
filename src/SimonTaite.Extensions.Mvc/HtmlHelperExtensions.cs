d5b0725a184a2f286b8c0c269fcd515332e95ed6
Simon Reynolds <simon.reynolds7@gmail.com>
2016-05-10 14:46:49 +0100

    Update project version and add license before publishing

--- src/SimonTaite.Extensions.Mvc/HtmlHelperExtensions.cs
+++ src/SimonTaite.Extensions.Mvc/HtmlHelperExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics.Contracts;
 using System.Linq.Expressions;
 using Microsoft.AspNet.Mvc.ViewFeatures;
 
@@ -9,16 +8,28 @@ namespace SimonTaite.Extensions.Mvc
     {
         public static string FieldIdFor<T, TResult>(this HtmlHelper<T> htmlHelper, Expression<Func<T, TResult>> expression)
         {
-            Contract.Requires<ArgumentNullException>(htmlHelper != null);
-            Contract.Requires<ArgumentNullException>(expression != null);
+            if (htmlHelper == null)
+            {
+                throw new ArgumentNullException(nameof(htmlHelper), @"htmlHelper cannot be null");
+            }
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression), @"expression cannot be null");
+            }
             
             return htmlHelper.GenerateIdFromName(htmlHelper.FieldNameFor(expression));
         }
         
         public static string FieldNameFor<T, TResult>(this HtmlHelper<T> htmlHelper, Expression<Func<T, TResult>> expression)
         {
-            Contract.Requires<ArgumentNullException>(htmlHelper != null);
-            Contract.Requires<ArgumentNullException>(expression != null);
+            if (htmlHelper == null)
+            {
+                throw new ArgumentNullException(nameof(htmlHelper), @"htmlHelper cannot be null");
+            }
+            if (expression == null)
+            {
+                throw new ArgumentNullException(nameof(expression), @"expression cannot be null");
+            }
             
             return htmlHelper.ViewData.TemplateInfo.GetFullHtmlFieldName(ExpressionHelper.GetExpressionText(expression));
         }
