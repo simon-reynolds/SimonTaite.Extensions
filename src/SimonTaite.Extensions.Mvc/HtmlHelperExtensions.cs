8070e17ff54a79e10ba45269044e411db1ccdcd3
Simon Taite <simontaite1@gmail.com>
2018-11-27 14:53:41 +0000

    Push v1.0.0
    Add appveyor.yaml

--- src/SimonTaite.Extensions.Mvc/HtmlHelperExtensions.cs
+++ src/SimonTaite.Extensions.Mvc/HtmlHelperExtensions.cs
@@ -7,7 +7,16 @@ namespace SimonTaite.Extensions.Mvc
 {
     public static class HtmlHelperExtensions
     {
-        public static string FieldIdFor<T, TResult>(this HtmlHelper<T> htmlHelper, Expression<Func<T, TResult>> expression)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+        /// <param name="expression">An expression that identifies the object that contains the ID.</param>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <returns>The HTML ID attribute value for the object that is represented by the expression.</returns>
+        [Obsolete("Use HtmlHelper's own IdFor extension method. This is provided for backwards compatability only")]
+        public static string FieldIdFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
         {
             if (htmlHelper == null)
             {
@@ -21,7 +30,16 @@ namespace SimonTaite.Extensions.Mvc
             return htmlHelper.IdFor(expression);
         }
         
-        public static string FieldNameFor<T, TResult>(this HtmlHelper<T> htmlHelper, Expression<Func<T, TResult>> expression)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper instance that this method extends.</param>
+        /// <param name="expression">An expression that identifies the object that contains the name.</param>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <typeparam name="TProperty">The type of the property.</typeparam>
+        /// <returns>The full HTML field name for the object that is represented by the expression.</returns>
+        [Obsolete("Use HtmlHelper's own NameFor extension method. This is provided for backwards compatability only")]
+        public static string FieldNameFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression)
         {
             if (htmlHelper == null)
             {
